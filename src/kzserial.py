9974fec5d9048d53345f4001985fd5da038032a3
ikz87 <iikz87ii@gmail.com>
2023-06-26 01:11:05 -0600

    Stop using dataclasses

--- src/kzserial.py
+++ src/kzserial.py
@@ -1,10 +1,7 @@
-from os import read
 import serial
 import sys
 import glob
 import json
-from dataclasses import dataclass, is_dataclass, field, asdict
-from dacite import from_dict
 
 
 def get_serial_ports():
@@ -41,57 +38,6 @@ def read_dict_from_port(port):
     line = port.readline().decode()
     return json.loads(line)
 
-
-@dataclass
-class GeneralConfigs:
-    rapid_trigger: bool
-    sensitivity: float
-    top_deadzone: float
-    bootom_deadzone: float
-    actuation_point: float
-    actuation_reset: float
-
-
-@dataclass
-class KeyActions:
-    actions: list[list[str]]
-
-
-@dataclass
-class Configs:
-    general: GeneralConfigs
-    key_1: KeyActions
-    key_2: KeyActions
-    key_3: KeyActions
-    key_4: KeyActions
-    key_5: KeyActions
-    key_6: KeyActions
-    key_7: KeyActions
-    key_8: KeyActions
-    key_9: KeyActions
-
-
-
-@dataclass
-class KeyState:
-    state: bool
-    distance: float
-
-
-@dataclass
-class Info:
-    temperature: float
-    key_1: KeyState
-    key_2: KeyState
-    key_3: KeyState
-    key_4: KeyState
-    key_5: KeyState
-    key_6: KeyState
-    key_7: KeyState
-    key_8: KeyState
-    key_9: KeyState
-
-
 def get_response_from_request(port, request):
     """
     Sends a request to the pico and waits until
@@ -100,11 +46,6 @@ def get_response_from_request(port, request):
     """
     port.write((request + "\n").encode())
 
-    constructor = {
-        "info_request": Info,
-        "configs_request": Configs,
-    }
-
     # Wait for a message that matches the request
     data = read_dict_from_port(port)
-    return from_dict(constructor[request], data=data)
+    return data
