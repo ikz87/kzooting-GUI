332ff8be3ed7810e22bfdc5e6b16ab260a510bf7
ikz87 <iikz87ii@gmail.com>
2023-06-28 02:31:20 -0600

    Lots of things about styling and stuff

--- src/main.py
+++ src/main.py
@@ -79,18 +79,18 @@ class GeneralInfo(QWidget):
         # Set up info strings
         info_strings = QLabel()
         info_strings.setText(
-            "Internal temperature: 0°C"
+            "Internal temperature: 0.00°C"
         )
 
         state.attach_listener(
             "info", lambda info:
                 info_strings.setText(
-                    "Internal temperature: " + str(info["temperature"]) + "°C"))
+                    "Internal temperature: " + str(round(info["temperature"],1)) + "°C"))
 
         # Populate grid
         general_info_grid = QGridLayout()
-        general_info_grid.addWidget(ports_menu, 0, 0, 1, 2)
-        general_info_grid.addWidget(info_strings, 1, 1)
+        general_info_grid.addWidget(ports_menu, 0, 0)
+        general_info_grid.addWidget(info_strings, 1, 0)
         self.setLayout(general_info_grid)
 
 
@@ -111,6 +111,7 @@ class GeneralConfigs(QWidget):
         actuation_method_vbox = QVBoxLayout()
         actuation_method_vbox.addWidget(rapid_trigger_rb)
         actuation_method_vbox.addWidget(fixed_actuation_rb)
+        actuation_method_vbox.addStretch()
 
         actuation_method = QGroupBox("Actuation method")
         actuation_method.setLayout(actuation_method_vbox)
@@ -150,6 +151,7 @@ class GeneralConfigs(QWidget):
         rt_options_vbox.addWidget(top_deadzone)
         rt_options_vbox.addWidget(bottom_deadzone_label)
         rt_options_vbox.addWidget(bottom_deadzone)
+        rt_options_vbox.addStretch()
 
         rt_options = QGroupBox("Rapid trigger options")
         rt_options.setLayout(rt_options_vbox)
@@ -161,7 +163,7 @@ class GeneralConfigs(QWidget):
         actuation_point.setRange(0.3, 3.5)
         actuation_point.setSingleStep(0.05)
         actuation_point_label = QLabel()
-        actuation_point_label.setText("Actuation_point")
+        actuation_point_label.setText("Actuation point")
         actuation_point_label.setBuddy(actuation_point)
 
         actuation_reset = QDoubleSpinBox()
@@ -170,7 +172,7 @@ class GeneralConfigs(QWidget):
         actuation_reset.setRange(0.1, 1)
         actuation_reset.setSingleStep(0.05)
         actuation_reset_label = QLabel()
-        actuation_reset_label.setText("Actuation_reset")
+        actuation_reset_label.setText("Actuation reset")
         actuation_reset_label.setBuddy(actuation_reset)
 
         fa_options_vbox = QVBoxLayout()
@@ -181,6 +183,7 @@ class GeneralConfigs(QWidget):
 
         fa_options = QGroupBox("Fixed actuation options")
         fa_options.setLayout(fa_options_vbox)
+        fa_options_vbox.addStretch()
 
         # Populate grid
         sub_options = QStackedWidget()
@@ -193,8 +196,6 @@ class GeneralConfigs(QWidget):
         self.setLayout(general_configs_grid)
 
         def update_options_from_dict(configs):
-            print(configs["general"]["rapid_trigger"])
-            print(rapid_trigger_rb.isChecked())
             sensitivity.setValue(configs["general"]["sensitivity"])
             top_deadzone.setValue(configs["general"]["top_deadzone"])
             bottom_deadzone.setValue(configs["general"]["bottom_deadzone"])
@@ -216,7 +217,6 @@ class GeneralConfigs(QWidget):
             state.out_configs["general"]["bottom_deadzone"] = bottom_deadzone.value()
             state.out_configs["general"]["actuation_point"] = actuation_point.value()
             state.out_configs["general"]["actuation_reset"] = actuation_reset.value()
-            print(state.out_configs)
 
             # Update group box that is shown
             if rapid_trigger_rb.isChecked():
@@ -275,6 +275,9 @@ class Visualizer(QWidget):
         bar.setMinimum(0)
         bar.setInvertedAppearance(True)
         bar_label = QLabel()
+        bar_label.setStyleSheet(
+            "font-size: 11px;"
+        )
         bar_label.setText("0.0 mm")
         bar_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         bar_label.setBuddy(bar)
@@ -282,32 +285,53 @@ class Visualizer(QWidget):
         bar_vbox.addWidget(bar_label)
         bar_vbox.addWidget(bar)
 
-        def update_bar_contents(info):
-            bar.setValue(round(info[state.key_selected]["distance"] * 100))
-            bar_label.setText("{:.1f}".format(info[state.key_selected]["distance"]))
-
-            # Update color
-            if info[state.key_selected]["state"]:
-                bar.setStyleSheet(
-                    "QProgressBar::chunk{background-color: #9cff8f}")
-            else:
-                bar.setStyleSheet(
-                    "QProgressBar::chunk{background-color: #ff8f8f}")
-
-        state.attach_listener(
-            "info", update_bar_contents)
-
-
         # Set up switch icon
-        switch_icon = QPixmap("../assets/switch.png")
-        switch_label = QLabel()
-        switch_label.setPixmap(switch_icon)
+        # The switch is divided in 3 to create an
+        # up and down motion
+        switch_top = QPixmap("../assets/switch_top.png").scaledToWidth(130)
+        switch_top_label = QLabel()
+        switch_top_label.setPixmap(switch_top)
+
+        switch_mid = QPixmap("../assets/switch_mid.png").scaledToWidth(130)
+        switch_mid_label = QLabel()
+        switch_mid_label.setPixmap(switch_mid)
+        switch_mid_label.setFixedHeight(50)
+
+        switch_bottom = QPixmap("../assets/switch_bottom.png").scaledToWidth(130)
+        switch_bottom_label = QLabel()
+        switch_bottom_label.setPixmap(switch_bottom)
+        switch_bottom_label.setStyleSheet(
+            "padding: 0 0 15 0"
+        )
+
+        switch_vbox = QVBoxLayout()
+        switch_vbox.addStretch()
+        switch_vbox.addWidget(switch_top_label)
+        switch_vbox.addWidget(switch_mid_label)
+        switch_vbox.addWidget(switch_bottom_label)
 
         # Populate grid
         grid = QGridLayout()
         grid.addLayout(bar_vbox, 0, 0)
-        grid.addWidget(switch_label, 0, 1)
+        grid.addLayout(switch_vbox, 0, 1)
         self.setLayout(grid)
+
+        def update_bar_contents(info):
+            distance = info[state.key_selected]["distance"]
+            bar.setValue(round(distance * 100))
+            bar_label.setText("{:.1f}".format(distance) + "mm" )
+            switch_mid_label.setFixedHeight(round(50-50*distance/4))
+
+            # Update color
+            if info[state.key_selected]["state"]:
+                bar.setStyleSheet(
+                    "QProgressBar::chunk{background-color: #B2CFFF}")
+            else:
+                bar.setStyleSheet(
+                    "QProgressBar::chunk{background-color: #555555}")
+
+        state.attach_listener(
+            "info", update_bar_contents)
 
 
 class MainWindow(QMainWindow):
@@ -332,19 +356,44 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.root_window)
 
         # Create widgets
+        switch_icon = QPixmap("../assets/switch.png")
+        switch_label = QLabel()
+        switch_label.setPixmap(switch_icon)
         general_info = GeneralInfo(state)
         general_configs = GeneralConfigs(state)
         key_configs = KeyConfigs(state)
         visualizer = Visualizer(state)
+
+        # Lower side
         save_button = QPushButton("&Save", self)
         save_button.clicked.connect(self.send_configs_to_pico)
+        kz_icon = QPixmap("../assets/kz_icon.jpeg").scaled(32,32)
+        kz_label = QLabel()
+        kz_label.setStyleSheet(
+            "border: 2px solid #B2CFFF;"
+        )
+        kz_label.setFixedWidth(36)
+        kz_label.setPixmap(kz_icon)
+
+        degen_icon = QPixmap("../assets/degen_icon.png").scaled(32,32)
+        degen_label = QLabel()
+        degen_label.setStyleSheet(
+            "border: 2px solid #B2CFFF;"
+        )
+        degen_label.setFixedWidth(36)
+        degen_label.setPixmap(degen_icon)
+
+        lower_side = QHBoxLayout()
+        lower_side.addWidget(kz_label)
+        lower_side.addWidget(save_button)
+        lower_side.addWidget(degen_label)
 
         # Populate grid
         self.root_grid.addWidget(general_info, 0, 0)
         self.root_grid.addWidget(general_configs, 1, 0)
         self.root_grid.addWidget(key_configs, 0, 1)
         self.root_grid.addWidget(visualizer, 1, 1)
-        self.root_grid.addWidget(save_button, 2, 0, 1, 2)
+        self.root_grid.addLayout(lower_side, 2, 0, 1, 2)
 
         # Watch for changes in ports directory
         self.rpp = None
@@ -374,7 +423,7 @@ class MainWindow(QMainWindow):
 
         timer2 = QTimer(self)
         timer2.timeout.connect(self.get_info_from_pico)
-        timer2.start(10)
+        timer2.start(16)
 
     def send_configs_to_pico(self):
         """
